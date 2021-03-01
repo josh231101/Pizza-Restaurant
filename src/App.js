11824736f0dd7380f3b3b71b89eec75c8210c2ac
Josué Arreola <wiijosue333@gmail.com>
2021-03-01 08:15:33 -0600

    Create products component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import Navbar from "./components/Navbar";
 import {BrowserRouter as Router} from 'react-router-dom';
 import { GlobalStyle } from "./globalStyles";
 import Hero from "./components/Hero";
+import Products from "./components/Products";
+import { productData } from "./components/Products/data";
 const App = ()=> {
   return (
     <Router className="App">
       <GlobalStyle/>
       <Hero/>
+      <Products heading='Choose your favorite' data={productData}/>
     </Router>
   );
 }
