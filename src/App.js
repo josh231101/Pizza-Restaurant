9229cafb128a3fdcd850386a58fd3c2a24e066e3
Josué Arreola <wiijosue333@gmail.com>
2021-03-01 13:03:16 -0600

    Create new section

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import { GlobalStyle } from "./globalStyles";
 import Hero from "./components/Hero";
 import Products from "./components/Products";
 import { productData } from "./components/Products/data";
+import Features from "./components/Features";
 const App = ()=> {
   return (
     <Router className="App">
       <GlobalStyle/>
       <Hero/>
       <Products heading='Choose your favorite' data={productData}/>
+      <Features/>
+
     </Router>
   );
 }
---
+++ src/components/Features/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { FeaturedButton, FeaturedContainer } from './FeaturesComponents';
+
+const Features = () => {
+    return (
+        <FeaturedContainer>
+            <h1>Pizza of the Day</h1>
+            <p>Truffle alfredo sauce topped with 23 carat gold trust</p>
+            <FeaturedButton>Order Now</FeaturedButton>
+        </FeaturedContainer>
+    )
+}
+
+export default Features
